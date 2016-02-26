51fa9d4784da1b0ce58daf0e5acefe552573a750
Ben Hoelzel <ben_hoelzel@cox.net>
2016-02-26 11:13:36 -0800

    WTF3.0

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once 'app/init.php';
 
 if(isset($_GET['query_set'])){
 
-// stuff...
+// Ok, here we go now....
   $query_data = $_GET['query_set'];
   $query_result = $client->search([
     'index' => 'sentiment',
