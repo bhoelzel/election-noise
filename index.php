3c5181a2ff1255662c266dac071dc660ea921d34
Ben Hoelzel <ben_hoelzel@cox.net>
2016-02-26 14:40:42 -0800

    Maybe???

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once 'app/init.php';
 
 if(isset($_GET['query_set'])){
 
-//Deploy from this brach?
+//Did we deploy properlly?
   $query_data = $_GET['query_set'];
   $query_result = $client->search([
     'index' => 'sentiment',
