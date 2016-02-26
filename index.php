ef8c41941088451e81f2a56db36a1fc8b1c4d507
Ben Hoelzel <ben_hoelzel@cox.net>
2016-02-26 11:19:43 -0800

    Tears are falling

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once 'app/init.php';
 
 if(isset($_GET['query_set'])){
 
-// Ok, here we go now 2.00
+// Ok, tears now
   $query_data = $_GET['query_set'];
   $query_result = $client->search([
     'index' => 'sentiment',
