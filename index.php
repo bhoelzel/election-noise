cd42b7b1462c7f8f06fd99a6366b959c5f9b554a
Ben Hoelzel <ben_hoelzel@cox.net>
2016-02-26 11:26:58 -0800

    ???

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once 'app/init.php';
 
 if(isset($_GET['query_set'])){
 
-// Ok, tears now
+// Ok, ??
   $query_data = $_GET['query_set'];
   $query_result = $client->search([
     'index' => 'sentiment',
